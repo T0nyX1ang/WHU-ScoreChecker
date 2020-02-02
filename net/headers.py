a500da5a41fd69f54829acdc2231a574586bee8a
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2020-02-02 16:12:47 +0800

    Update headers.py
    Remove cache-control in the headers

--- net/headers.py
+++ net/headers.py
@@ -6,7 +6,6 @@ class Headers(object):
 			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36", 
 			"Connection": "keep-alive", 
 			"Accept-Encoding": "gzip, deflate", 
-			"Cache-Control": "max-age=0", 
 			"DNT": "1", 
 			"Host": "bkjw.whu.edu.cn", 
 			"Upgrade-Insecure-Requests": "1", 
