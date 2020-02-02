ef42ae478c31752db7e75b6e49a0e9013b137f0c
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2020-02-03 00:27:28 +0800

    Update loader.py
    Add a query model loader (working on it now)

--- model/loader.py
+++ model/loader.py
@@ -1,14 +1,31 @@
-from keras.models import load_model
-
 # model loader
-# models must be put in the model folder.
 
-def load(filename):
+def load_captcha_model(filename):
+	from keras.models import load_model
 	try:
-		print('Trying to load the model ...')
+		print('Trying to load the captcha model ...')
 		model = load_model(filename)
 		print('Model is loaded successfully ...')
 		return model
 	except Exception as e:
 		print('Failed to load the model:', e)
 		return None
+
+def validate_query_model(model):
+	return True
+
+def load_query_model(filename):
+	import json
+	try:
+		print('Trying to load the query model ...')
+		with open(filename, 'r', encoding='utf-8') as f:
+			fin = f.read()
+			model = json.loads(fin)
+		if validate_query_model(model):
+			print('Model is loaded successfully ...')
+			return model
+		else:
+			return None
+	except Exception as e:
+		print('Failed to load the model:', e)
+		return None		
