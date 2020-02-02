75926d7ce023b4145d4533901e8aa4d8f4098264
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2020-02-03 00:28:56 +0800

    Enhance security and robustness in this app
    private variables and status checks

--- config.py
+++ config.py
@@ -3,76 +3,112 @@ import json
 import tkinter
 import tkinter.filedialog
 import tkinter.messagebox
+from model import loader
 
 class ConfigApp(object):
-	# main GUI for the score checker app
+	# GUI for the config app.
+	# Need to be called before login and query.
 
 	def __init__(self):
-		self._CONFIG_FILENAME = 'config.json'
-		self._main_window = tkinter.Tk()
-		self._main_window.title("Configurations")
-		self._main_window.geometry("300x150+600+300")
-		self._ID = tkinter.StringVar()
-		self._password = tkinter.StringVar()
-		self._model_dir = tkinter.StringVar()
-		self.__on_load()
-		self._ID_label = tkinter.Label(self._main_window, text="ID")
-		self._ID_label.grid(row=0, column=0, padx=20, pady=10)
-		self._ID_entry = tkinter.Entry(self._main_window, textvariable=self._ID)
-		self._ID_entry.grid(row=0, column=1, padx=20, pady=10)
-		self._password_label = tkinter.Label(self._main_window, text="Password")
-		self._password_label.grid(row=1, column=0, padx=20, pady=10)
-		self._password_entry = tkinter.Entry(self._main_window, textvariable=self._password)
-		self._password_entry.grid(row=1, column=1, padx=20, pady=10)
-		self._button_frame = tkinter.Frame(self._main_window)
-		self._button_frame.grid(row=2, column=0, columnspan=2, padx=20, pady=10)
-		self._model_entry = tkinter.Button(self._button_frame, text='Load Model', command=self.__load_model)
-		self._model_entry.pack(side='left', padx=10)
-		self._save_button = tkinter.Button(self._button_frame, text='   Save   ', command=self.__save)
-		self._save_button.pack(side='left', padx=10)
-		self._exit_button = tkinter.Button(self._button_frame, text='   Exit   ', command=self.__exit)
-		self._exit_button.pack(side='left', padx=10)
+		# Initialization for the GUI. All of the variables are private here.
+		self.__main_window = tkinter.Tk()
+		self.__main_window.title("Configurations")
+		self.__main_window.geometry("400x150+600+300")
+		self.__status = False
 
-	def __on_load(self):
-		if os.path.exists(self._CONFIG_FILENAME):
-			with open(self._CONFIG_FILENAME, 'r') as f:
-				json_file = f.read()
-			try:
-				config = json.loads(json_file)
-				self._ID.set(config['ID'])
-				self._password.set(config['Password'])
-				self._model_dir.set(config['Model Directory'])
-				print('Previous configuration file has been loaded successfully...')
-			except Exception as e:
-				self._ID.set("")
-				self._password.set("")
-				self._model_dir.set("")
-				print('Configuration file has been corrupted, removing it...')
-				os.remove(self._CONFIG_FILENAME)
+		self.__ID = ''
+		self.__ID_label = tkinter.Label(self.__main_window, text="ID")
+		self.__ID_label.grid(row=0, column=0, padx=20, pady=10)
+		self.__ID_entry = tkinter.Entry(self.__main_window, width=30)
+		self.__ID_entry.grid(row=0, column=1, padx=20, pady=10)
 
-	def __load_model(self):
-		model_filename = tkinter.filedialog.askopenfilename(filetypes=[('HDF5 file', '.hdf5')], title='Load Model')
-		from model import loader
-		model = loader.load(model_filename)
-		if model is not None:
-			self._model_dir.set(model_filename)
+		self.__password = ''
+		self.__password_label = tkinter.Label(self.__main_window, text="Password")
+		self.__password_label.grid(row=1, column=0, padx=20, pady=10)
+		self.__password_entry = tkinter.Entry(self.__main_window, width=30)
+		self.__password_entry.grid(row=1, column=1, padx=20, pady=10)
+
+		self.__button_frame = tkinter.Frame(self.__main_window)
+		self.__button_frame.grid(row=2, column=0, columnspan=2, padx=20, pady=10)
+		self.__captcha_model = None
+		self.__captcha_model_button = tkinter.Button(self.__button_frame, text='Load Captcha Model', command=self.__load_captcha_model)
+		self.__captcha_model_button.pack(side='left', padx=10)
+		self.__query_model = None
+		self.__query_model_button = tkinter.Button(self.__button_frame, text='Load Query Model', command=self.__load_query_model)
+		self.__query_model_button.pack(side='left', padx=10)
+		self.__proceed_button = tkinter.Button(self.__button_frame, text='Proceed', command=self.__proceed)
+		self.__proceed_button.pack(side='left', padx=10)
+
+		self.__main_window.mainloop()
+
+	def __load_captcha_model(self):
+		# Load a captcha model, should be HDF5 format.
+		model_filename = tkinter.filedialog.askopenfilename(filetypes=[('HDF5 format', '.hdf5')], title='Load Captcha Model')
+		self.__captcha_model = loader.load_captcha_model(model_filename)
+		if self.__captcha_model is None:
+			tkinter.messagebox.showerror("Load Captcha Model", "Failed to load the captcha model, please retry.")
 		else:
-			tkinter.messagebox.showerror("Load Model", "Failed to load the model, please retry.")
+			tkinter.messagebox.showinfo("Load Captcha Model", "The captcha model is loaded successfully.")
 
-	def __save(self):
-		config = {}
-		config['ID'] = self._ID_entry.get()
-		config['Password'] = self._password_entry.get()
-		config['Model Directory'] = self._model_dir.get()
-		with open(self._CONFIG_FILENAME, 'w') as f:
-			f.write(json.dumps(config, indent=4))
+	def __load_query_model(self):
+		# Load a query model, should be JSON format.
+		model_filename = tkinter.filedialog.askopenfilename(filetypes=[('JSON format', '.json')], title='Load Query Model')
+		self.__query_model = loader.load_query_model(model_filename)
+		if self.__query_model is None:
+			tkinter.messagebox.showerror("Load Query Model", "Failed to load the query model, please retry.")
+		else:
+			tkinter.messagebox.showinfo("Load Query Model", "The query model is loaded successfully.")
 
-	def __exit(self):
-		self._main_window.quit()
+	def __proceed(self):
+		# Process the variables passed in.
+		status = True
+		fail_hint = 'Failed to proceed:' + os.linesep
+		if len(self.__ID_entry.get()) == 0: 
+			status = False
+			fail_hint += 'Your ID should not be empty.' + os.linesep
+		else:
+			self.__ID = self.__ID_entry.get()
 
-	def run(self):
-		self._main_window.mainloop()
+		if len(self.__password_entry.get()) == 0:
+			status = False
+			fail_hint += 'Your password should not be empty.' + os.linesep
+		else:
+			self.__password = self.__password_entry.get()
+
+		if self.__captcha_model is None:
+			status = False 
+			fail_hint += 'You should load a captcha model to recognize the captcha.' + os.linesep
+
+		if self.__query_model is None:
+			status = False
+			fail_hint += 'You should load a query model to query your score table.' + os.linesep
+
+		if status:
+			print('Status checkes have been passed ...')
+			self.__main_window.quit()
+		else:
+			print('Some checkes have been failed ...')
+			tkinter.messagebox.showerror("Proceed", fail_hint)
+
+		self.__status = status
+
+	def get_status(self):
+		# Call this function before you call get_credentials.
+		# A PermissionError will be thrown probably if you don't do the check here.
+		return self.__status
+
+	def get_credentials(self):
+		# This should be the only data connection between this app and the caller.
+		# For security, this function should be called after status flag have been set to True.
+		# Once called, the status flag is set to False.
+		# Please handle it with care.
+		if self.get_status():
+			self.__status = False # reset status flag
+			return self.__ID, self.__password, self.__captcha_model, self.__query_model
+		else:
+			raise PermissionError('Your access to this function violates the security policy here.')
 
 if __name__ == '__main__':
 	app = ConfigApp()
-	app.run()
+	if app.get_status():
+		print(app.get_credentials())
