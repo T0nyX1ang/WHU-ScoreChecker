ea820e1d4476caea75c983727f1718695ea48e3c
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2020-02-04 22:04:49 +0800

    Update query.py
    Shorten code length

--- course/query.py
+++ course/query.py
@@ -1,93 +1,46 @@
 import time
 
 # base query library to search desired items.
-# available queries: year_range, score_come_out, score_range, credit, study_type, course_type
-# auxiliary function: subtract
+# available queries: start_year, stop_year, 
+#					 score_come_out, min_score, max_score, 
+#					 min_credit, max_credit, 
+#					 study_type, course_type,
+#					 course_academy, course_name
 
-def subtract(alist, blist):
-	# subtract a 'list' from another 'list'
-	clist = []
-	for val in alist:
-		if val not in blist:
-			clist.append(val)
-	return clist
+def search_start_year(score_table, start_year):
+	return [val for val in score_table if (int(val[5]), int(val[6])) >= tuple(start_year)]
 
-def search_year_range(score_table, start=None, stop=None):
-	# search the year range between start and stop
-	# start and stop are tuples: (year, semester), and their default values are based on your local time
-	if start is None:
-		start = (str(time.localtime().tm_year - 7), '1')
-	if stop is None:
-		stop = (str(time.localtime().tm_year), '3')
-	result_table = []
-	for table_row in score_table:
-		if start <= (table_row[5], table_row[6]) <= stop:
-			result_table.append(table_row)
-	return result_table
+def search_stop_year(score_table, stop_year):
+	return [val for val in score_table if (int(val[5]), int(val[6])) <= tuple(stop_year)]
 
 def search_score_come_out(score_table, come_out=True):
-	# search the scores that has come out
-	result_table = []
-	for table_row in score_table:
-		if (come_out and len(table_row[7]) != 0) or (not come_out and len(table_row[7]) == 0):
-			result_table.append(table_row)
-	return result_table
+	if come_out:
+		return [val for val in score_table if len(val[7]) != 0]
+	else:
+		return [val for val in score_table if len(val[7]) == 0]
 
-def search_score_range(score_table, lowerbound=0.0, upperbound=100.0):
-	# search the score range between lowerbound and upperbound
-	score_table = search_score_come_out(score_table, come_out=True) # Only find score that comes out
-	result_table = []
-	for table_row in score_table:
-		if lowerbound <= float(table_row[7]) <= upperbound:
-			result_table.append(table_row)
-	return result_table
+def search_min_score(score_table, min_score):
+	score_come_out = search_score_come_out(score_table, come_out=True)
+	return [val for val in score_table if float(val[7]) >= min_score]
 
-def search_credit_range(score_table, lowerbound=0.0, upperbound=8.0):
-	# search the credit range between lowerbound and upperbound
-	result_table = []
-	for table_row in score_table:
-		if lowerbound <= float(table_row[2]) <= upperbound:
-			result_table.append(table_row)
-	return result_table
+def search_max_score(score_table, max_score):
+	score_come_out = search_score_come_out(score_table, come_out=True)
+	return [val for val in score_table if float(val[7]) <= max_score]
 
-def search_study_type(score_table, study_type=None):
-	# search study type
-	if study_type is None:
-		return score_table # no type will be regarded as wide types
-	result_table = []
-	for table_row in score_table:
-		if table_row[4] == study_type:
-			result_table.append(table_row)
-	return result_table
+def search_min_credit(score_table, min_credit):
+	return [val for val in score_table if float(val[2]) >= min_credit]
 
-def search_course_type(score_table, course_type=None):
-	# search course type
-	if course_type is None:
-		return score_table # no type will be regarded as wide types
-	result_table = []
-	for table_row in score_table:
-		if table_row[1] == course_type:
-			result_table.append(table_row)
-	return result_table
+def search_max_credit(score_table, max_credit):
+	return [val for val in score_table if float(val[2]) <= max_credit]
 
-def search_course_academy(score_table, academy):
-	# search academy (precise)
-	# may search for a long time
-	if academy is None or len(academy) >= 15:
-		return []
-	result_table = []
-	for table_row in score_table:
-		if table_row[3] == academy:
-			result_table.append(table_row)
-	return result_table
+def search_study_type(score_table, study_type):
+	return [val for val in score_table if val[4] in study_type]
 
-def search_course(score_table, course):
-	# search course name (precise)
-	# may search for a long time
-	if course is None or len(course) >= 15:
-		return []
-	result_table = []
-	for table_row in score_table:
-		if table_row[0] == course:
-			result_table.append(table_row)
-	return result_table
+def search_course_type(score_table, course_type):
+	return [val for val in score_table if val[1] in course_type]
+
+def search_course_academy(score_table, course_academy):
+	return [val for val in score_table if val[3] in course_academy]
+
+def search_course_name(score_table, course_name):
+	return [val for val in score_table if val[0] in course_name]
