0fa11fbb85fafa2a3e266e387df1e573d8758646
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2020-02-05 13:05:52 +0800

    Move predict into image part

--- image/predict.py
+++ image/predict.py
@@ -1,9 +1,10 @@
 import os, numpy
-from image import process, partition
+from .process import preprocess, resize, otsu
+from .partition import partition
 
 # predict the captcha
 
-def predict(bytes_im, model):
+def predict_captcha(bytes_im, model):
 	captcha = ''
 	INV_TRANSFORM_STR = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
 	if bytes_im is None:
@@ -11,12 +12,12 @@ def predict(bytes_im, model):
 
 	if model is not None:
 		array_im = numpy.asarray(bytearray(bytes_im), dtype='uint8')
-		proc_im = process.preprocess(array_im)
-		parts = partition.partition(proc_im)
+		proc_im = preprocess(array_im)
+		parts = partition(proc_im)
 		for val in parts:
 			crop = proc_im[val[1]:val[1] + val[3], val[0]:val[0] + val[2]]
-			letter = process.resize(crop, width=20, height=20)
-			letter = process.otsu(letter) # threshold again for more simple image
+			letter = resize(crop, width=20, height=20)
+			letter = otsu(letter) # threshold again for more simple image
 			letter = numpy.expand_dims(letter, axis=2) // 255
 			letter = numpy.expand_dims(letter, axis=0)
 			prediction = model.predict(letter) # predict the letter
