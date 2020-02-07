8f1136227bd193fe72607d547ce44fad71121e24
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2020-02-07 22:23:17 +0800

    Fix lint errors

--- course/extract.py
+++ course/extract.py
@@ -1,7 +1,24 @@
-from .query import *
-from .optimize import *
+"""
+Extract information from a score table.
+
+Available functions: convert, extract
+"""
+
+from .query import search_start_year, search_stop_year, \
+    search_score_come_out, search_min_score, search_max_score, \
+    search_min_credit, search_max_credit, \
+    search_study_type, search_course_type, \
+    search_course_academy, search_course_name
+
 
 def convert(key):
+    """
+    Convert keys to their functions.
+
+    Available keys are: start_year, stop_year,
+    score_come_out, min_score, max_score, min_credit, max_credit
+    study_type, course_type, course_academy, course_name
+    """
     lookup_table = {
         'start_year': lambda s, v: search_start_year(s, v),
         'stop_year': lambda s, v: search_stop_year(s, v),
@@ -17,19 +34,23 @@ def convert(key):
     }
     return lookup_table[key]
 
-def extract(score_table, query_model):
-    # main module, the results will be sent to the result app.
-    # for convenience, we use literal_eval to transform a str to a function.
-    # please note: if you want to use the model separately, you must validate the model for security issues.
 
+def extract(score_table, query_model):
+    """
+    Extract required information from a score table based on the query model.
+
+    This is the main module, the results will be sent to the result app.
+    If you want to use the model separately, you must validate the model.
+    """
     if not query_model.get('display'):
         return score_table
 
-    middle_extract = {} # store for later use
+    middle_extract = {}  # store for later use
     for item in query_model['query'].keys():
-        temp_score_table = score_table  
+        temp_score_table = score_table
         for each_key in query_model['query'][item].keys():
-            temp_score_table = convert(each_key)(temp_score_table, query_model['query'][item][each_key])
+            temp_score_table = convert(each_key)(
+                temp_score_table, query_model['query'][item][each_key])
         middle_extract[item] = temp_score_table
 
     return middle_extract[query_model['display']]
