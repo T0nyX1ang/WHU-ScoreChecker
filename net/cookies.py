afc3127924bea9a6ddb1313389dc292c08fbf9c1
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2020-02-08 15:36:09 +0800

    Fix lint issues and add docs

--- net/cookies.py
+++ net/cookies.py
@@ -1,15 +1,41 @@
+"""
+Process cookies during HTTP requests.
+
+The module works like a simple session, and set some initial cookies.
+The CookieJar in requests.cookies is used in this module.
+"""
+
 import requests.cookies
 
+
 class Cookies(object):
-    # Basic cookies part.
+    """
+    Basic cookies class.
+
+    Contains initial cookies, cookies updating and current cookies fetching.
+    """
+
     def __init__(self):
+        """
+        Initialization.
+
+        For each single session, please call this once.
+        """
         self.__cookies = requests.cookies.RequestsCookieJar()
         self.__cookies['userLanguage'] = 'zh-CN'
-        
+
     def update(self, new_cookies):
-        # update the cookie jar with the response data
+        """
+        Update the cookie jar with the response data.
+
+        The new_cookies should be a requests HTTP request with cookies.
+        """
         self.__cookies.update(new_cookies)
 
     def current(self):
-        # return current cookies
+        """
+        Fetch current cookies.
+
+        Invoke this function while constucting an HTTP request.
+        """
         return self.__cookies
