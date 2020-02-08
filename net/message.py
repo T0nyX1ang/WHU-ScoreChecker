a847a91abb104d7af92e4bd9d840d3ef789625d1
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2020-02-08 16:02:07 +0800

    Fix lint issues and add docs

--- net/message.py
+++ net/message.py
@@ -1,21 +1,50 @@
+"""
+Process continous HTTP requests.
+
+The module works like a simple session with headers and cookies updated
+automatically.
+"""
+
 import requests
 from .headers import Headers
 from .cookies import Cookies
 
+
 class Message(object):
-    # a wrapper for requests, trying to make use of the headers and cookies
+    """
+    Basic message class.
+
+    A wrapper for requests, trying to make use of the headers and cookies.
+    """
+
     def __init__(self):
+        """
+        Initialization for initial headers and cookies.
+
+        Based on the cookies and headers class.
+        """
         self.__headers, self.__cookies = Headers(), Cookies()
 
     def get(self, url, timeout=2):
-        # HTTP GET request
+        """
+        Make HTTP GET requests.
+
+        'url' is the URL you want to make a request.
+        'timeout' is set to 2.0 seconds on default.
+        """
         try:
             print('Making a HTTP GET request ...')
-            self.__headers.update('Content-Type') # Remove Content-Type header when GET
-            self.__headers.update('Origin') # Remove Origin header when GET
-            response = requests.get(url=url, timeout=timeout, headers=self.__headers.current(), cookies=self.__cookies.current())
-            self.__cookies.update(response.cookies) # Update cookies
-            # self.__headers.update('Referer', response.url) # Update referrer
+            # Remove Content-Type and Origin header when GET
+            self.__headers.update('Content-Type')
+            self.__headers.update('Origin')
+            response = requests.get(
+                url=url,
+                timeout=timeout,
+                headers=self.__headers.current(),
+                cookies=self.__cookies.current()
+            )
+            self.__cookies.update(response.cookies)  # Update cookies
+            # self.__headers.update('Referer', response.url)
             content = response.content
             print('HTTP GET response is received successfully ...')
             return content
@@ -24,14 +53,28 @@ class Message(object):
             return None
 
     def post(self, url, data, timeout=2):
-        # HTTP POST request
+        """
+        Make HTTP POST requests.
+
+        'url' is the URL you want to make a request.
+        'timeout' is set to 2.0 seconds on default.
+        """
         try:
             print('Making a HTTP POST request ...')
-            self.__headers.update('Content-Type', 'application/x-www-form-urlencoded') # Add Content-Type header when POST
-            self.__headers.update('Origin', self.__headers.current()['Host']) # Add Origin header when POST
-            response = requests.post(url=url, data=data, timeout=timeout, headers=self.__headers.current(), cookies=self.__cookies.current())
-            self.__cookies.update(response.cookies) # update cookies
-            # self.__headers.update('Referer', response.url) # Update referrer
+            # Add Content-Type and Origin header when GET
+            self.__headers.update('Content-Type',
+                                  'application/x-www-form-urlencoded'
+                                  )
+            self.__headers.update('Origin',
+                                  self.__headers.current()
+                                  ['Host'])
+            response = requests.post(url=url,
+                                     data=data,
+                                     timeout=timeout,
+                                     headers=self.__headers.current(),
+                                     cookies=self.__cookies.current())
+            self.__cookies.update(response.cookies)  # update cookies
+            # self.__headers.update('Referer', response.url)
             content = response.content
             print('HTTP POST response is received successfully ...')
             return content
