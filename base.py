adfeb9e17bc5cbf60d2d9d0981284d9d4090c274
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2020-02-08 16:15:03 +0800

    Fix lint issues and add docs

--- base.py
+++ base.py
@@ -1,9 +1,26 @@
+"""
+Base app defines the title and geometry settings of an app.
+
+This module should better be inherited by other apps.
+"""
+
 import tkinter
 
+
 class BaseApp(tkinter.Tk):
-    # This is the base app which create center tkinter layouts
-    # To reuse it, make it inherited by other app files
+    """
+    Base app which create center tkinter layouts.
+
+    This class inherits tkinter.Tk module.
+    To reuse it, make it inherited by other apps.
+    """
+
     def __init__(self, title, width, height):
+        """
+        Initialize with title, width and height.
+
+        Invoke this function to deal with layout settings.
+        """
         super(BaseApp, self).__init__()
         self.title(title)
         self.__geometry(width, height)
@@ -12,8 +29,5 @@ class BaseApp(tkinter.Tk):
         # put window in the center of the screen
         offset_width = (self.winfo_screenwidth() - width) // 5 * 2
         offset_height = (self.winfo_screenheight() - height) // 5 * 2
-        self.geometry("%sx%s+%s+%s" % (width, height, offset_width, offset_height))
-        
-if __name__ == '__main__':
-    app = BaseApp('hello', 400, 300)
-    app.mainloop()
+        self.geometry("%sx%s+%s+%s" %
+                      (width, height, offset_width, offset_height))
