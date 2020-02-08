e6ddb4e5c36c846a6f3ff97f9c8ee6dbb71f1e29
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2020-02-08 11:11:03 +0800

    Fix lint issues and add docs

--- image/predict.py
+++ image/predict.py
@@ -1,12 +1,26 @@
-import os, numpy
+"""
+Predict the captcha from an image downloaded from a certain pattern.
+
+The pattern is based on the captcha model.
+The captcha model should be a Keras-loaded model Object that has a function
+'predict'.
+This function utilized partition and process function.
+"""
+
+import numpy
 from .process import preprocess, resize, otsu
 from .partition import partition
 
-# predict the captcha
 
 def predict_captcha(bytes_im, model):
+    """
+    Main predict function.
+
+    'bytes_im' is a bytes image which is downloaded on Internet.
+    'model' is a Keras sequence model Object.
+    """
     captcha = ''
-    INV_TRANSFORM_STR = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+    convert = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
     if bytes_im is None:
         print('Please check your network.')
 
@@ -17,10 +31,10 @@ def predict_captcha(bytes_im, model):
         for val in parts:
             crop = proc_im[val[1]:val[1] + val[3], val[0]:val[0] + val[2]]
             letter = resize(crop, width=20, height=20)
-            letter = otsu(letter) # threshold again for more simple image
+            letter = otsu(letter)  # threshold again for more simple image
             letter = numpy.expand_dims(letter, axis=2) // 255
             letter = numpy.expand_dims(letter, axis=0)
-            prediction = model.predict(letter) # predict the letter
-            captcha += INV_TRANSFORM_STR[numpy.argmax(prediction)]
-    
+            prediction = model.predict(letter)  # predict the letter
+            captcha += convert[numpy.argmax(prediction)]
+
     return captcha
