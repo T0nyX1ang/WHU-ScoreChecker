a70921489b084d79c12ed2f7c5fc427c9c346c00
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2020-02-11 16:03:15 +0800

    Replace the __login function with a separate auth module

---
+++ app/auth.py
@@ -0,0 +1,66 @@
+"""
+Authentication app.
+
+Deal with user's login and logout actions.
+"""
+
+import time
+from image import predict
+from net import message, analyze
+
+
+def auth_procedure(_id, password, captcha_model, retries=3):
+    """
+    A authentication module for user. Perform multiple authentications.
+
+    Any exceptions will be caught.
+    If a valid score table is returned, no exceptions will be caught.
+    Otherwise, it will return None for nothing.
+    'retries' show the number of retrying attempts.
+    """
+    score_table = None
+
+    while retries >= 0:
+        print('Authenticating, remaining retries:', retries)
+        url = 'http://bkjw.whu.edu.cn'  # url
+        msg_handler = message.Message()  # message control
+        try:
+            # fetch and predict captcha
+            base_content = msg_handler.get(url=url)
+            captcha_id = analyze.get_captcha_id(base_content)
+            captcha_url = url + captcha_id
+            captcha_content = msg_handler.get(url=captcha_url)
+            captcha = predict.predict_captcha(captcha_content, captcha_model)
+
+            # fetch login_id and log in
+            login_id = analyze.get_login_id(base_content)
+            login_url = url + login_id
+            login_data = {
+                'timestamp': int(time.time() * 1000),
+                'jwb': '%E6%AD%A6%E5%A4%A7%E6%9C%AC%E7%A7%91%E6%95%99%E5%8A\
+%A1%E7%B3%BB%E7%BB%9F',
+                'id': _id,
+                'pwd': password,
+                'xdvfb': captcha,
+            }
+            login_content = msg_handler.post(url=login_url, data=login_data)
+
+            # fetch csrf token and score table
+            csrf_token = analyze.get_csrf_token(login_content)
+            score_table_url = url + '/servlet/Svlt_QueryStuScore?csrftoken=' \
+                + csrf_token + '&year=0&term=&learnType=&scoreFlag=0&t=' \
+                + str(time.ctime()).replace('  ', ' ') + ' GMT 0800 (中国标准时间)'
+            score_table_content = msg_handler.get(url=score_table_url)
+            score_table = analyze.get_score_table(score_table_content)
+
+            # log out for next-time use
+            logout_url = url + '/servlet/logout'
+            msg_handler.get(url=logout_url)
+            return score_table
+        except Exception as e:
+            print('Failed to authenticate:', e)
+            print('Retrying ...')
+            retries -= 1
+
+    # this will happen if retries = 0
+    print('Failed to authenticate, please check your connection.')
--- app/main.py
+++ app/main.py
@@ -5,11 +5,9 @@ This is the CORE part of this project and should be the only file to be
 executed when you are just a user. This file connects all the files needed.
 """
 
-import time
+from auth import auth_procedure
 from config import ConfigApp
 from course import extract
-from image import predict
-from net import message, analyze
 from result import ResultApp
 
 
@@ -20,13 +18,13 @@ class ScoreCheckerApp(object):
     This is the main part to deal with everything.
     """
 
-    def __init__(self):
+    def __init__(self, retries=3):
         """
         Initialization for the app.
 
         Just invoke this function to use it.
         """
-        self._retries = 3  # global retry counter
+        self.__retries = retries  # global retry counter
         self.__control()
 
     def __control(self):
@@ -40,57 +38,20 @@ class ScoreCheckerApp(object):
         else:
             return
 
-        score_table = None
-        while self._retries >= 0 and not score_table:
-            print('Logging in, remaining retries:', self._retries)
-            score_table = self.__login(_id, password, captcha_model)
-            self._retries -= 1
-
-        if self._retries < 0:
-            print('Failed to log in, please check your connection.')
+        score_table = auth_procedure(_id, password, captcha_model,
+                                     retries=self.__retries)
+        if not score_table:
             return
 
-        print('Logging in successfully, querying ...')
+        print('Authenticating successfully, querying ...')
         self.__query(score_table, query_model)
         print('Querying successfully, see you next time.')
-
-    def __login(self, _id, password, captcha_model):
-        # A login module.
-        url = 'http://bkjw.whu.edu.cn'  # url
-        msg_handler = message.Message()  # message control
-        try:
-            base_content = msg_handler.get(url=url)
-            captcha_id = analyze.get_captcha_id(base_content)
-            captcha_url = url + captcha_id
-            captcha_content = msg_handler.get(url=captcha_url)
-            captcha = predict.predict_captcha(captcha_content, captcha_model)
-            login_id = analyze.get_login_id(base_content)
-            login_url = url + login_id
-            login_data = {
-                'timestamp': int(time.time() * 1000),
-                'jwb': '%E6%AD%A6%E5%A4%A7%E6%9C%AC%E7%A7%91%E6%95%99%E5%8A%A1\
-%E7%B3%BB%E7%BB%9F',
-                'id': _id,
-                'pwd': password,
-                'xdvfb': captcha,
-            }
-            login_content = msg_handler.post(url=login_url, data=login_data)
-            csrf_token = analyze.get_csrf_token(login_content)
-            score_table_url = url + '/servlet/Svlt_QueryStuScore?csrftoken=' \
-                + csrf_token + '&year=0&term=&learnType=&scoreFlag=0&t=' \
-                + str(time.ctime()).replace('  ', ' ') + ' GMT 0800 (中国标准时间)'
-            score_table_content = msg_handler.get(url=score_table_url)
-            score_table = analyze.get_score_table(score_table_content)
-            return score_table
-        except Exception as e:
-            print('Failed to log in:', e)
-            print('Retrying ...')
-            return None
 
     def __query(self, score_table, query_model):
         # A query module.
         result_score_table = extract.extract(score_table, query_model)
-        ResultApp(result_score_table)
+        rapp = ResultApp(result_score_table)
+        rapp.run()
 
 
 if __name__ == '__main__':
