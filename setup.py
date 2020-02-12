a234ffbcdf0d10044a2f03dadb0fa317cf90f630
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2020-02-12 15:58:49 +0800

    Make the project into a package

---
+++ setup.py
@@ -0,0 +1,43 @@
+"""
+Package setup.
+
+This file is used to package the whole project.
+"""
+
+import setuptools
+
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='WHU-ScoreChecker',
+    version='1.0.1',
+    author='Tony Xiang',
+    author_email='tonyxfy@qq.com',
+    description='A simple, open-source and model-driven score checker for \
+WHU.',
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/T0nyX1ang/WHU-ScoreChecker',
+    packages=setuptools.find_packages(include=['scorechecker',
+                                               'scorechecker.*']),
+    package_data={
+        'static': ['*.json']
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+        "Development Status :: 4 - Beta",
+    ],
+    python_requires='>=3.5, <3.8',
+    entry_points={
+        'console_scripts': [
+            'scorechecker=scorechecker.main:run',
+        ],
+    }
+)
